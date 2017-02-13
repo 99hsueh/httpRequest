d7fec3cdc4b0b1e93e049a52dd93696683cc3631
99hsueh <99hhsueh@gmail.com>
2017-02-13 20:33:54 +0000

    use require and export of files

---
+++ printDataStep5export.js
@@ -0,0 +1,17 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+  https.get(options,(getHTML) => {
+    getHTML.setEncoding('utf8');
+
+    let contentBody = '';
+
+    getHTML.on('data', function(chunk) {
+      contentBody += chunk;
+    });
+
+    getHTML.on('end', () => {
+      callback(contentBody)
+    });
+  });
+}
